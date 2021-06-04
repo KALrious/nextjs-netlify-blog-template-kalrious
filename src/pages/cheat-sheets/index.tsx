291b2d516d47cd5fc4a730aa178b626baa8b6ddb
Jean Gautier <jgautier@oui.sncf>
2021-06-04 10:42:44 +0200

    feat: add special navigation for cheat sheet

--- src/pages/cheat-sheets/index.tsx
+++ src/pages/cheat-sheets/index.tsx
@@ -1,7 +1,17 @@
 import Layout from "../../components/Layout";
+import BasicMeta from "../..//components/meta/BasicMeta";
+import OpenGraphMeta from "../..//components/meta/OpenGraphMeta";
+import TwitterCardMeta from "../..//components/meta/TwitterCardMeta";
 
-const Index = () => (
-    <Layout>TOTO</Layout>
-)
+const Index = () => {
+    const url = "/cheat-sheet";
+    const title = "All cheat sheet";
+    return(
+        <Layout>
+            <BasicMeta url={url} title={title} />
+            <OpenGraphMeta url={url} title={title} />
+            <TwitterCardMeta url={url} title={title} />TOTO</Layout>
+    )
+}
 
 export default Index;
