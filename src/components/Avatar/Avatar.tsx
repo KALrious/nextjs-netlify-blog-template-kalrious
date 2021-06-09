1be91ce69b73f2e793a1a96bb984031746538ffe
KALrious <j.gautier180293+git@gmail.com>
2021-06-09 16:38:04 +0200

    add props to component without props

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
 import styles from 'Avatar.module.scss';
 
-const Avatar: FC = () => {
+const Avatar: FC = ({ children }) => {
   return (
     <div>
       <img src="images/kalrious.png" />
+      {children}
     </div>
   );
 };
